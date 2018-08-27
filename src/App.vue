5292156bcee0a30c86d05ba8ee9301f8708aaf97
Teemu_Karppinen <karppinenteemu@outlook.com>
2018-08-27 22:21:48 +0300

    Move misplaced file

--- src/App.vue
+++ src/App.vue
@@ -45,7 +45,7 @@ body {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
-    background-image: url("../static/img/bgPlaceHold.png");
+    background-image: url("../assets/bgPlaceHold.png");
 }
 
 .nav-bar {
